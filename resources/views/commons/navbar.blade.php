b4a363867477cbf40c6b5b71b2f32f757d697648
naoto.tanabe <kotikame00@gmail.com>
2021-12-30 05:57:24 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -6,20 +6,31 @@
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
         </button>
-
+        
         <div class="collapse navbar-collapse" id="nav-bar">
+    @if (Auth::check())
             <ul class="navbar-nav mr-auto">
-                {{-- ユーザページのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">ユーザページ</a></li>
-                {{-- 習慣新規作成ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">習慣新規作成</a></li>
+                    {{-- ユーザページのリンク --}}
+                    <li class="nav-item"><a href="#" class="nav-link">ユーザページ</a></li>
+                    {{-- 習慣新規作成ページへのリンク --}}
+                    <li class="nav-item"><a href="#" class="nav-link">習慣新規作成</a></li>
+            </ul>   
+            <ul class="navbar-nav　ml-auto">
+                    {{-- ログアウトへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('logout.get', 'ログアウト', ) !!}</li>
             </ul>
-            <ul class="navbar-nav">
-                {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
-                {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">新規アカウント作成</a></li>
-            </ul>
+            
+                
+    @else
+                    <ul class="navbar-nav ml-auto">
+                        {{-- ログインページへのリンク --}}
+                        <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+                        {{-- ユーザ登録ページへのリンク --}}
+                        <li class="nav-item">{!! link_to_route('signup.get', 'アカウント新規作成', [], ['class' => 'nav-link']) !!}</li>
+                    </ul>
+    
+    @endif        
         </div>
+      
     </nav>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>ようこそ Good Habitへ</h1>
-            <button type="button" class="btn btn-primary btn-sm">アカウント新規作成</button>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>ようこそ Good Habitへ</h1>
+                <br>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
